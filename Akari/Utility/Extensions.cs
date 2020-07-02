63a6a922f96271bdabd8e991a1e521eab232a185
Z3RYX <zeryxofficial@gmail.com>
2020-07-02 22:43:57 +0200

    Added CDN Endpoints

---
+++ Akari/Utility/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akari.Utility
+{
+    public enum CDNExtension
+    {
+        JPEG,
+        PNG,
+        WebP,
+        GIF
+    }
+}
