2f559ef4a10bbb627ae3c9d77a6608518a5e266c
Z3RYX <zeryxofficial@gmail.com>
2020-10-20 11:33:42 +0200

    Added interface for ImageData class

---
+++ Yamashiro/Models/IImageData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yamashiro.Utility;
+
+namespace Yamashiro.Models
+{
+    public interface IImageData
+    {
+        public string RawData { get; set; }
+        public CDNExtension FileType { get; }
+        public string Base64File { get; }
+    }
+}
